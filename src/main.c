3d6897b43e3765d1a75c8afd9be29b961a53ae67
fred-boy <fred-boy@protonmail.com>
2018-02-28 14:57:25 +0700

    Add placeholder for input parsing

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static int GetLine (char *buff, size_t sz) {
+static int GetLine(char *buff, size_t sz) {
     int ch, extra;
     if (fgets (buff, sz, stdin) == NULL) {
         return 1;
@@ -19,9 +19,58 @@ static int GetLine (char *buff, size_t sz) {
     return 0;
 }
 
+void MakeMove(char *move, int m1, int m2, int pawn) {
+    int l1 = (int)(move[m1+1-pawn] - 'a');
+    int l2 = (int)(move[m2-2] - 'a');
+    int d1 = (int)(move[m1+2-pawn] - '1');
+    int d2 = (int)(move[m2-1] - '1');
+    board[l2][d2] = board[l1][d1];
+    board[l1][d1] = ' ';
+}
+
+void MakeKill() {
+    int l1 = (int)(move[m1+1-pawn] - 'a');
+    int l2 = (int)(move[m2-2] - 'a');
+    int d1 = (int)(move[m1+2-pawn] - '1');
+    int d2 = (int)(move[m2-1] - '1');
+    board[l2][d2] = board[l1][d1];
+    board[l1][d1] = ' ';
+}
+
+void MakeTurn() {
+    int i,sp;
+    char turn[64];
+    while (GetLine(turn, sizeof(turn))) {}
+    for (i = 0; i < strlen(turn); i++){
+        if (turn[i] == ' ') {
+            sp = i;
+            break;
+        }
+    }
+    for (i = 0; i < strlen(turn); i++){
+        if (turn[i] == '-') {
+            if (i < sp) {
+                MakeMove(turn, 0, sp, (i == 2));
+            } else {
+                MakeMove(turn, sp+1, strlen(turn), (i == sp+3));
+            }
+        } else if (turn[i] == 'x') {
+            if (i < sp) {
+                MakeMove(turn, 0, sp, (i == 2));
+            } else {
+                MakeMove(turn, sp+1, strlen(turn), (i == sp+3));
+            }
+        }
+    }
+}
+
 int main() {
     InitBoard();
-    PrintBoard();
+    while(1) {
+        system("clear");
+        PrintBoard();
+        MakeTurn();
+    }
     ClearBoard();
     return 0;
 }
