cf9681e06f6b1ab6d0f0f568aa87d89c02d7344d
fred-boy <fred-boy@protonmail.com>
2018-02-28 14:19:26 +0700

    Add function to read input

--- src/main.c
+++ src/main.c
@@ -1,5 +1,24 @@
 #include "board.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static int GetLine (char *buff, size_t sz) {
+    int ch, extra;
+    if (fgets (buff, sz, stdin) == NULL) {
+        return 1;
+    }
+    if (buff[strlen(buff)-1] != '\n') {
+        extra = 0;
+        while (((ch = getchar()) != '\n') && (ch != EOF))
+            extra = 1;
+        return (extra == 1) ? 1 : 0;
+    }
+    buff[strlen(buff)-1] = '\0';
+    return 0;
+}
+
 int main() {
     InitBoard();
     PrintBoard();
