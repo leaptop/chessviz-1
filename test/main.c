4aabd367f4d6c5f526ce4f2d30a8c28a6a3143f1
fred-boy <fred-boy@protonmail.com>
2018-03-07 04:49:46 +0700

    Add simple test

--- test/main.c
+++ test/main.c
@@ -1,6 +1,15 @@
 #define CTEST_MAIN
 
+#include <funcs.h>
+#include <board.h>
+
 #include <ctest.h>
+
+CTEST(simple_suite, is_char_in_bounds_test) {
+    const int result = InBounds('b','a','c');
+    const int expected = 1;
+    ASSERT_EQUAL(expected, result);
+}
 
 int main(int argc, const char** argv) {
     return ctest_main(argc, argv);
